91f4a2a970eda1f7d369f5d0dc976e4901e55f2a
iniTwakkie <dnell.personal@gmail.com>
2024-08-27 09:50:33 +0100

    testing logic on datatables

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+import pandas as pd
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -7,9 +8,22 @@ print(load_dotenv())
 server = Flask(__name__)
 
 @server.route("/")
-def hello():
-    return "Hello, World!"
+def base():
+    return render_template("base.html")
 
 @server.route("/blank")
-def test():
+def login():
     return "Hello, World2!"
+
+@server.route("/example1")
+def example1():
+    df = pd.read_csv('./static/dataset/results.csv')
+    data = df.head(10000)
+    data = data.to_dict(orient='records')
+    return render_template("example1.html", data_t=data)
+
+@server.route("/example2")
+def example2():
+    df = pd.read_csv('./static/dataset/results.csv')
+    data = df.head(10)
+    return render_template("example2.html", data_t=data)
---
+++ static/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('#example').DataTable(),
+    $('#example1').DataTable();
+});
+
+	
+// new DataTable('#example2');
+
+
+let table = new DataTable('#example2', {
+    // options
+});
+
+// $(document).ready(function() {
+//     $('#example2').DataTable(); // Initialize DataTables for any table with id 'example'
+// });
